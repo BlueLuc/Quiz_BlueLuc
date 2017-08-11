168e7274f99162c1f1bf5296d8f2e13e34414dee
Luca Horinek <luca@Lucas-MacBook-Pro.local>
2017-08-11 11:48:01 +0200

    Version 2

--- start-quiz.php
+++ start-quiz.php
@@ -4,16 +4,289 @@
         <title>Quiz über Mich!</title>
             <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
             <link rel="icon" href="favicon.ico" type="image/x-icon">
+            <link rel="stylesheet" href="css/vendor/bootstrap.css">
             <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
-        <h1>Ein Quiz über Mich</h1>
-        <div class="row quiz-options">
-            <div class="col-md-3">
-                <form action="quiz_1.php">
-                    <input type="submit" value="Start" class="btn btn-primary"/>
-                </form>
+        <div class="container">
+
+            <h1>Ein Quiz über Mich</h1>
+            <div class="row quiz-options">
+                <div class="col-md-3">
+
+                    <form action="" method="post">
+                        <div class="quiz-question">
+                            <h2>Wann habe ich Geburtstag?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="24.07.2001" required />
+                                        <label for="question-1-answers-A">24.07.2001</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="25.04.2002" />
+                                        <label for="question-1-answers-A">25.04.2002</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-1-answers" id="question-1-answers-c" value="30.06.2003" />
+                                        <label for="question-1-answers-A">30.06.2003</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="24.07.2001" />
+                                        <label for="question-1-answers-A">15.02.2001</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Was sind meine lieblings Farben?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-2-answers" id="question-1-answers-A" value="Grün, Rot, Blau" required />
+                                        <label for="question-1-answers-A">Grün, Rot, Blau</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-2-answers" id="question-1-answers-B" value="Blau, Schwarz, Gelb" />
+                                        <label for="question-1-answers-A">Blau, Schwarz, Gelb </label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-2-answers" id="question-1-answers-C" value="Orange, Blau, Rot" />
+                                        <label for="question-1-answers-A">Orange, Blau, Rot</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-2-answers" id="question-1-answers-D" value="Blau, Grün, Schwarz" />
+                                        <label for="question-1-answers-A">Blau, Grün, Schwarz</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Wie viele Follower habe ich auf Instagram?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-3-answers" id="question-1-answers-A" value="1-100" required />
+                                        <label for="question-1-answers-A">1-100</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-3-answers" id="question-1-answers-B" value="100-200" />
+                                        <label for="question-1-answers-A">100-200 </label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-3-answers" id="question-1-answers-C" value="300-400" />
+                                        <label for="question-1-answers-A">300-400</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-3-answers" id="question-1-answers-D" value="600+" />
+                                        <label for="question-1-answers-A">600+</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>In welche Volkschule ging ich?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-4-answers" id="question-1-answers-A" value="Notre Dame de Sion" required />
+                                        <label for="question-1-answers-A">Notre Dame de Sion</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-4-answers" id="question-1-answers-B" value="Volkschule Kenyongasse" />
+                                        <label for="question-1-answers-A">Volkschule Kenyongasse</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-4-answers" id="question-1-answers-C" value="Volkschule Corneliusgasse" />
+                                        <label for="question-1-answers-A">Volkschule Corneliusgasse</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-4-answers" id="question-1-answers-D" value="Volkschule Neustiftgasse" />
+                                        <label for="question-1-answers-A">Volkschule Neustiftgasse</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Von welcher Marke war mein erstes Handy?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-5-answers" id="question-1-answers-A" value="Apple" required />
+                                        <label for="question-1-answers-A">Apple</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-5-answers" id="question-1-answers-B" value="Samsung" />
+                                        <label for="question-1-answers-A">Samsung</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-5-answers" id="question-1-answers-C" value="Nokia" />
+                                        <label for="question-1-answers-A">Nokia</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-5-answers" id="question-1-answers-D" value="Sony" />
+                                        <label for="question-1-answers-A">Sony</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Wie heißt mein bester Freund?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-6-answers" id="question-1-answers-A" value="Markus" required />
+                                        <label for="question-1-answers-A">Markus</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-6-answers" id="question-1-answers-B" value="Alexander" />
+                                        <label for="question-1-answers-A">Alexander</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-6-answers" id="question-1-answers-C" value="Bernhard" />
+                                        <label for="question-1-answers-A">Bernhard</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-6-answers" id="question-1-answers-D" value="Luis" />
+                                        <label for="question-1-answers-A">Luis</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Welche App benütze ich am öftesten am Handy?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-7-answers" id="question-1-answers-A" value="Snapchat" required />
+                                        <label for="question-1-answers-A">Snapchat</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-7-answers" id="question-1-answers-B" value="Facebook" />
+                                        <label for="question-1-answers-A">Facebook</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-7-answers" id="question-1-answers-C" value="Instagram" />
+                                        <label for="question-1-answers-A">Instagram</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-7-answers" id="question-1-answers-D" value="Twitter" />
+                                        <label for="question-1-answers-A">Twitter</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Welchen Fußballverein mag ich am liebsgten?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-8-answers" id="question-1-answers-A" value="FAK Austriat" required />
+                                        <label for="question-1-answers-A">FAK Austria</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-8-answers" id="question-1-answers-B" value="SK Rapid" />
+                                        <label for="question-1-answers-A">SK Rapid</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-8-answers" id="question-1-answers-C" value="Real Madrid" />
+                                        <label for="question-1-answers-A">Real Madrid</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-8-answers" id="question-1-answers-D" value="FC Barcelona" />
+                                        <label for="question-1-answers-A">FC Barcelona</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Wie heißt mein Kater?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-9-answers" id="question-1-answers-A" value="Finn" required />
+                                        <label for="question-1-answers-A">Finn</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-9-answers" id="question-1-answers-B" value="Tiger" />
+                                        <label for="question-1-answers-A">Tiger</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-9-answers" id="question-1-answers-C" value="Fluffy" />
+                                        <label for="question-1-answers-A">Fluffy</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-9-answers" id="question-1-answers-D" value="Leo" />
+                                        <label for="question-1-answers-A">Leo</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quiz-question">
+                            <h2>Welches Wort konnte ich als erstes sagen?</h2>
+                            <div class="row quiz-options">
+                                <div class="col-md-12">
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-10-answers" id="question-1-answers-A" value="Mama" required />
+                                        <label for="question-1-answers-A">Mama</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-10-answers" id="question-1-answers-B" value="Oma" />
+                                        <label for="question-1-answers-A">Oma</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-10-answers" id="question-1-answers-C" value="Papa" />
+                                        <label for="question-1-answers-A">Papa</label>
+                                    </div>
+                                    <div class="quiz-question-answer">
+                                        <input type="radio" name="question-10-answers" id="question-1-answers-D" value="Ball" />
+                                        <label for="question-1-answers-A">Ball</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <input type="submit" value="Abschicken" class="btn btn-primary" />
+                    </form>
+
+
+                </div>
             </div>
+
+            <div class="row quiz-options">
+                <?php
+
+                $answer1 = $_POST['question-1-answers'];
+                $answer2 = $_POST['question-2-answers'];
+                $answer3 = $_POST['question-3-answers'];
+                $answer4 = $_POST['question-4-answers'];
+                $answer5 = $_POST['question-5-answers'];
+                $answer6 = $_POST['question-6-answers'];
+                $answer7 = $_POST['question-7-answers'];
+                $answer8 = $_POST['question-8-answers'];
+                $answer9 = $_POST['question-9-answers'];
+                $answer10 = $_POST['question-10-answers'];
+
+                $totalCorrect = 0;
+
+                if ($answer1 == "25.04.2002") { $totalCorrect++; }
+                if ($answer2 == "Blau, Grün, Schwarz") { $totalCorrect++; }
+                if ($answer3 == "300-400") { $totalCorrect++; }
+                if ($answer4 == "Notre Dame de Sion") { $totalCorrect++; }
+                if ($answer5 == "Nokia") { $totalCorrect++; }
+                if ($answer6 == "Luis") { $totalCorrect++; }
+                if ($answer7 == "Snapchat") { $totalCorrect++; }
+                if ($answer8 == "SK Rapid") { $totalCorrect++; }
+                if ($answer9 == "Leo") { $totalCorrect++; }
+                if ($answer10 == "Ball") { $totalCorrect++; }
+
+                echo "<div id='results'>$totalCorrect / 10 correct</div>";
+
+                ?>
+            </div>
+
+
+
         </div>
+
     </body>
 </html>
